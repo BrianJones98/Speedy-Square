945eb42612d582cd4dd25404ceea7d6228224706
Brian Jones <brian.r.jones.98@gmail.com>
2020-11-16 22:21:00 -0800

    added obstacle class and basic random obstacle functionality

--- data.py
+++ data.py
@@ -7,6 +7,9 @@ xInitial = screenSize[0]/4
 yInitial = screenSize[1]/2
 window = pygame.display.set_mode((screenSize[0], screenSize[1]))
 
+#index corresponds with an obstacle type (0: damage, 1: slow, 2: no dash)
+colorList = [(255, 0, 0), (255, 0, 255), (255, 255, 255)]
+
 class Player:
     def __init__(self, x, y, color):
         self.x = x
@@ -54,9 +57,28 @@ class Player:
         if self.cooldown > 0:
             self.cooldown -= 1
 
+class Obstacle:
+    def __init__(self, x, y, speed, objType, shape):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.objType = objType
+        self.color = colorList[objType]
+        self.size = [50, 50]
+        self.shape = shape
+
+    def draw(self, window):
+        if self.shape == 0:
+            pygame.draw.rect(window, self.color, (self.x, self.y, self.size[0], self.size[1]))
+        else:
+            pygame.draw.circle(window, self.color, (self.x, self.y), self.size[0])
+
 square = Player(xInitial, yInitial, (0, 255, 255))
+activeObstacles = []
 
 def redrawGameWindow():
     window.fill((0, 0, 0))
     square.draw(window)
+    for obstacle in activeObstacles:
+        obstacle.draw(window)
     pygame.display.update()
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from data import *
+import random
 
 pygame.init()
 pygame.display.set_caption("Speedy Square")
@@ -12,9 +13,24 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+    for obstacle in activeObstacles:
+        if obstacle.x < screenSize[0] and obstacle.x > 0:
+            obstacle.x += obstacle.speed
+        else:
+            activeObstacles.pop(activeObstacles.index(obstacle))
 
-    square.move(keys)
+    if len(activeObstacles) < 10:
+        objX = random.randint(0, screenSize[0])
+        objY = random.randint(0, screenSize[1])
+        objSpeed = random.randint(10, 50)
+        objType = random.randint(0, 2)
+        objShape = random.randint(0, 1)
+        
+        activeObstacles.append(Obstacle(objX, objY, objSpeed, objType, objShape))
+
+    direction = pygame.key.get_pressed()
+
+    square.move(direction)
 
     redrawGameWindow()
 
