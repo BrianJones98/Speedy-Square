17c894f77b8dd63cc35720e8224d2dc61aae9c5e
Brian Jones <brian.r.jones.98@gmail.com>
2020-11-17 21:03:25 -0800

    bunch of additions

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+pygame.display.set_caption("Speedy Square")
 
 screenSize = [1280, 720]
 
@@ -7,8 +10,17 @@ xInitial = screenSize[0]/4
 yInitial = screenSize[1]/2
 window = pygame.display.set_mode((screenSize[0], screenSize[1]))
 
+healthFont = pygame.font.SysFont('latin', 30, True)
+
+hitSound = pygame.mixer.Sound('hit.wav')
+backMusic = pygame.mixer.music.load('background.mp3')
+
+pygame.mixer.music.play(-1)
+
 #index corresponds with an obstacle type (0: damage, 1: slow, 2: no dash)
 colorList = [(255, 0, 0), (255, 0, 255), (255, 255, 255)]
+squareColor = (0, 0, 255)
+invincibleColor = (0, 255, 255)
 
 class Player:
     def __init__(self, x, y, color):
@@ -19,6 +31,10 @@ class Player:
         self.cooldown = 0
         self.dashTime = 0
         self.color = color
+        self.health = 5
+        self.hitRecovery = 0
+        self.invincible = False
+        self.debuffTimer = 0
     
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.size[0], self.size[1]))
@@ -49,36 +65,95 @@ class Player:
             self.speed *= 4
             self.dashTime = 5
             self.cooldown = 10
+    
+    def hit(self, hitType):
+        if self.invincible == False:
+            if hitType == 0:
+                self.health -= 1
+                self.hitRecovery = 10
+            elif hitType == 1:
+                self.speed /= 2
+                self.debuffTimer = 50
+                self.hitRecovery = 10
+            else:
+                self.cooldown = 50
+                self.hitRecovery = 10
+            
+            hitSound.play()
+        
+        if self.health <= 0:
+            freeze = True
+            while freeze:
+                pygame.time.delay(60)
+                
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                
+                lossMessage = healthFont.render("You died. Press space to restart.", 1, (255, 255, 255))
+                window.fill((0, 0, 0))
+                window.blit(lossMessage, (screenSize[0]/3, screenSize[1]/2))
+                checkInput = pygame.key.get_pressed()
 
-        if self.cooldown != 0 and self.dashTime > 0:
-            self.dashTime -= 1
-        if self.dashTime == 0:
-            self.speed = origSpeed
-        if self.cooldown > 0:
-            self.cooldown -= 1
+                if checkInput[pygame.K_SPACE]:
+                    levelReset()
+                    freeze = False
+                
+                pygame.display.update()
 
 class Obstacle:
-    def __init__(self, x, y, speed, objType, shape):
+    def __init__(self, x, y, speed, objType, height, width, direction):
         self.x = x
         self.y = y
         self.speed = speed
         self.objType = objType
         self.color = colorList[objType]
-        self.size = [50, 50]
-        self.shape = shape
+        self.height = height
+        self.width = width
+        self.direction = direction
 
     def draw(self, window):
-        if self.shape == 0:
-            pygame.draw.rect(window, self.color, (self.x, self.y, self.size[0], self.size[1]))
-        else:
-            pygame.draw.circle(window, self.color, (self.x, self.y), self.size[0])
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
 
-square = Player(xInitial, yInitial, (0, 255, 255))
+square = Player(xInitial, yInitial, squareColor)
 activeObstacles = []
 
 def redrawGameWindow():
     window.fill((0, 0, 0))
+    healthDisplay = healthFont.render(f"Health: {square.health}", 1, (255, 255, 255))
+    window.blit(healthDisplay, (10, 10))
     square.draw(window)
     for obstacle in activeObstacles:
         obstacle.draw(window)
     pygame.display.update()
+
+def dropTimers():
+    if square.debuffTimer > 0:
+        square.debuffTimer -= 1
+    if square.cooldown > 0:
+        square.cooldown -= 1
+    if square.dashTime > 0:
+        square.dashTime -= 1
+    if square.hitRecovery > 0:
+        square.hitRecovery -= 1
+
+def checkTimers():
+    if square.debuffTimer == 0 and square.dashTime == 0:
+        square.speed = origSpeed
+    if square.dashTime > 0 or square.hitRecovery > 0:
+        square.invincible = True
+    else:
+        square.invincible = False
+    if square.invincible:
+        square.color = invincibleColor
+    else:
+        square.color = squareColor
+
+def levelReset():
+    square.x = xInitial
+    square.y = yInitial
+    activeObstacles.clear()
+    square.health = 5
+    square.cooldown = 0
+    square.dashTime = 0
+    square.debuffTimer = 0
--- game.py
+++ game.py
@@ -1,36 +1,127 @@
 from data import *
 import random
 
-pygame.init()
-pygame.display.set_caption("Speedy Square")
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    # surface.blit(textobj, textrect)
 
-running = True
+click = False
+font = pygame.font.SysFont("comicsans", 70)
+def main_menu():
+    run = True
+    while run:
+        
+        window.fill((0,0,0))
+        draw_text('main menu', font, (255, 255, 255), screenSize, 20, 20)
+        
+        mx, my = pygame.mouse.get_pos()
+        
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(window, (255, 0, 0), button_1)
+        pygame.draw.rect(window, (255, 0, 0), button_2)
+        
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+                    
+        pygame.display.update()
 
-while running:
-    pygame.time.delay(60)
+def game():
+    running = True
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while running:
+        pygame.time.delay(60)
 
-    for obstacle in activeObstacles:
-        if obstacle.x < screenSize[0] and obstacle.x > 0:
-            obstacle.x += obstacle.speed
-        else:
-            activeObstacles.pop(activeObstacles.index(obstacle))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    if len(activeObstacles) < 10:
-        objX = random.randint(0, screenSize[0])
-        objY = random.randint(0, screenSize[1])
-        objSpeed = random.randint(10, 50)
-        objType = random.randint(0, 2)
-        objShape = random.randint(0, 1)
+        for obstacle in activeObstacles:
+            sqEndX = square.x + square.size[0]
+            sqEndY = square.y + square.size[1]
+            objEndX = obstacle.x + obstacle.width
+            objEndY = obstacle.y + obstacle.height
 
-        activeObstacles.append(Obstacle(objX, objY, objSpeed, objType, objShape))
+            if square.x > obstacle.x - square.size[0] and sqEndX < objEndX + square.size[0]:
+                if square.y > obstacle.y - square.size[1] and sqEndY < objEndY + square.size[1]:
+                    square.hit(obstacle.objType)
+            
+            if obstacle.x <= screenSize[0] and obstacle.x >= 0:
+                if obstacle.y <= screenSize[1] and obstacle.y >= 0:
+                    if obstacle.direction == 0:
+                        obstacle.x += obstacle.speed
+                    elif obstacle.direction == 1:
+                        obstacle.y += obstacle.speed
+                    elif obstacle.direction == 2:
+                        obstacle.x -= obstacle.speed
+                    else:
+                        obstacle.y -= obstacle.speed
+                else:
+                    activeObstacles.pop(activeObstacles.index(obstacle))
+            else:
+                activeObstacles.pop(activeObstacles.index(obstacle))
 
-    direction = pygame.key.get_pressed()
-    square.move(direction)
+        if len(activeObstacles) < 10:
+            # 0 right, 1 down, 2 left, 3 up
+            objDirection = random.randint(0, 3)
+            if objDirection == 0:
+                objX = 0
+                objY = random.randint(0, screenSize[1])
+            elif objDirection == 1:
+                objX = random.randint(0, screenSize[0])
+                objY = 0
+            elif objDirection == 2:
+                objX = screenSize[0]
+                objY = random.randint(0, screenSize[1])
+            else:
+                objX = random.randint(0, screenSize[0])
+                objY = screenSize[1]
+            
+            objSpeed = random.randint(10, 50)
+            objType = random.randint(0, 2)
+            objWidth = random.randint(10, 100)
+            objHeight = random.randint(10, 100)
 
-    redrawGameWindow()
+            activeObstacles.append(Obstacle(objX, objY, objSpeed, objType, objWidth, objHeight, objDirection))
 
-pygame.quit()
+        direction = pygame.key.get_pressed()
+        square.move(direction)
+
+        dropTimers()
+        checkTimers()
+
+        redrawGameWindow()
+
+    pygame.quit()
+
+def options():
+    running = True
+    while running:
+        draw_text('Options', font, (255, 255, 255), screenSize, 20, 20)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        pygame.display.update()
+
+main_menu()
